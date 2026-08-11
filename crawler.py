6f4bfd772bf473af58ae30fce2b8e223c22df7fe
Rohan Wandre <rohanwandre24@gmail.com>
2026-08-11 14:43:56 +0200

    increase playwright timeout

--- crawler.py
+++ crawler.py
@@ -6,11 +6,16 @@ def crawl_page(url):
     with sync_playwright() as p:
 
         browser = p.chromium.launch(
-            headless=True
+            headless=True,
+            args=[
+                "--no-sandbox",
+                "--disable-dev-shm-usage"
+            ]
         )
 
         context = browser.new_context(
-            ignore_https_errors=True
+            ignore_https_errors=True,
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/124.0.0.0 Safari/537.36"
         )
 
         page = context.new_page()
@@ -18,12 +23,10 @@ def crawl_page(url):
         page.goto(
             url,
             wait_until="domcontentloaded",
-            timeout=30000
+            timeout=120000
         )
 
-        page.wait_for_timeout(
-            3000
-        )
+        page.wait_for_timeout(5000)
 
         html = page.content()
 
